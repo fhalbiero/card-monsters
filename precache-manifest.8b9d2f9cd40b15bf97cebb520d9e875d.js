1bb4586193af2bfe9fe42ee80531951996053d91
fhalbiero <fhalbiero@gmail.com>
2019-09-10 16:34:33 -0300

    Updates

---
+++ precache-manifest.8b9d2f9cd40b15bf97cebb520d9e875d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cff50e399d2dcd8f6a6cdcea28b997",
+    "url": "/card-monsters/index.html"
+  },
+  {
+    "revision": "86167bd6da2c15116b57",
+    "url": "/card-monsters/static/css/main.22fbc6b2.chunk.css"
+  },
+  {
+    "revision": "93b91bababd5e2095107",
+    "url": "/card-monsters/static/js/2.522b3b10.chunk.js"
+  },
+  {
+    "revision": "86167bd6da2c15116b57",
+    "url": "/card-monsters/static/js/main.a9eecd3b.chunk.js"
+  },
+  {
+    "revision": "d3debce6d9903804ba22",
+    "url": "/card-monsters/static/js/runtime~main.7ff7e13a.js"
+  }
+]);
